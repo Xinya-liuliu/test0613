49f54de393d63abe9c9e6a36a07ef2575b41e385
Liu Xinya <liuxinya_hbu@163.com>
2022-06-12 20:36:15 +0800

    master

--- test1.py
+++ test1.py
@@ -1,2 +1,9 @@
 from importlib import import_module
+from multiprocessing.spawn import import_main_path
+from tkinter import image_names
 import_module
+import_main_path
+image_names
+
+#this is first try
+#this is second try
